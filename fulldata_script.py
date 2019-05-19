9c847eda71a8c5fc7f23ed3ce52abc2b12b00f39
ItsgNegrao <silvag.1996@alunos.utfpr.edu.br>
2019-05-19 16:53:55 -0300

    comentarios com lixo etc e ihul

--- fulldata_script.py
+++ fulldata_script.py
@@ -6,6 +6,54 @@ from subprocess import getoutput as gop
 import matplotlib.pyplot as plt
 import glob
 from copy import deepcopy
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import classification_report, accuracy_score, confusion_matrix
+from sklearn.model_selection import GridSearchCV, train_test_split
+from keras import regularizers
+from keras.models import Sequential
+from keras.layers import Dense, Dropout
+from keras.optimizers import SGD, Adam
+from keras.utils.np_utils import to_categorical
+
+def plot_history(h):
+    loss_list = [s for s in h.history.keys() if 'loss' in s and 'val' not in s]
+    val_loss_list = [s for s in h.history.keys() if 'loss' in s and 'val' in s]
+    acc_list = [s for s in h.history.keys() if 'acc' in s and 'val' not in s]
+    val_acc_list = [s for s in h.history.keys() if 'acc' in s and 'val' in s]
+    if len(loss_list) == 0:
+        print('Custo não está presente no histórico')
+        return
+    epochs = range(1, len(history.history[loss_list[0]]) + 1)
+    # Custo
+    plt.figure(1)
+    for l in loss_list:
+        plt.plot(epochs, h.history[l], 'b',
+                 label='Custo [treinamento] (' + str(str(format(
+                    h.history[l][-1],'.5f'))+')'))
+    for l in val_loss_list:
+        plt.plot(epochs, h.history[l], 'g',
+                 label='Custo [validação] (' + str(str(format(
+                    h.history[l][-1],'.5f'))+')'))
+    plt.title('Custo')
+    plt.xlabel('Épocas')
+    plt.ylabel('Custo')
+    plt.legend()
+    # Acurácia
+    plt.figure(2)
+    for l in acc_list:
+        plt.plot(epochs, h.history[l], 'b',
+                 label='Acurácia [treinamento] (' + str(format(
+                    h.history[l][-1],'.5f'))+')')
+    for l in val_acc_list:
+        plt.plot(epochs, h.history[l], 'g',
+                 label='Acurácia [validação] (' + str(format(
+                    h.history[l][-1],'.5f'))+')')
+    plt.title('Acurácia')
+    plt.xlabel('Épocas')
+    plt.ylabel('Acurácia')
+    plt.legend()
+    plt.show()
+
 
 # identificando pastas
 folders = {
@@ -55,29 +103,109 @@ for types in small_dir:
 data = np.array(subjects)
 print(data.shape)
 
-# Função auxilar que achou a pasta e arquivo com erro na base large (remoção de arquivo necessária)
-# for k in range(0,len(data)):
-#     for i in range(0,len(data[k])):
-#         if(len(data[k][i]) < 64):
-#             print("pasta: ",k)
-#             print("arquivo: ",i)
-#             print("len arquivo: ",len(data[k][i]))
-#             print("data", data[k][i])
+newData = list()
+for person in data:
+    newEletrodos = list()
+    for eletrodos in person:
+        newEletrodos.append(normalize(eletrodos, axis=0))
+    newData.append(np.array(newEletrodos))
+data = np.array(newData)
+print(data.shape)
 
 
-# Prepara info para os eletrodos selecionados
-# primeira sessão, primeiro trial
 newData = list()
 for person in data:
     for eletrodos in person:
-        newEletrodos = list()
-        for eletrodo in eletrodos:
-            newEletrodos.append(np.mean(eletrodo))
-        newData.append(newEletrodos)
-
-data = newData
+        newData.append(normalize(eletrodos, axis=0))
+data = np.array(newData)
 newData = None
+print(data.shape)
+print(ch_names)
 
+
+# ________________________________________________________________________
+# remmover X, nd e Y
+
+# definição de uma fração do regularizador
+l = 0.01
+
+# url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/car/car.data'
+# urlretrieve(url, 'car.txt')
+# filedata = open('car.txt')
+# data = filedata.read()
+# dataset = np.array([s.split(',') for s in data.split('\n')][:-1])
+# print(dataset)
+# print(len(dataset))
+# print(dataset.shape)
+
+# # Transformação dos valores de categórico para numérico
+# le = LabelEncoder()
+# features = np.array([le.fit_transform(f) for f in dataset[:, :-1].T]).T
+
+# # obtendo a coluna com as respostas
+# labels = le.fit_transform(dataset[:, -1])
+# categorical_labels = to_categorical(labels, num_classes=len(set(labels)))
+
+# # Dividindo em conjuntos de treino (80%) e teste (20%)
+# X_train, X_test, y_train, y_test = train_test_split(
+#     features, categorical_labels, test_size=0.3)
+
+# # treino: 80% dos 80% de treino. teste: 20% dos 80% de treino.
+# X_train, X_val, y_train, y_val = train_test_split(
+#     X_train, y_train, test_size=0.3, shuffle=True)
+
+# desenvolvimento do modelo Keras para uma MLP
+model = Sequential()
+model.add(Dense(570, activation='relu', input_shape=(64,256)))
+model.add(Dense(64, activation='relu'))
+model.add(Dense(2, activation='softmax'))
+
+# Aplicação de um modelo de descida de gradiente utilizando o Stocastic Gradient Descendent (SGD)
+sgd = SGD(lr=0.05, momentum=0.0)
+# Função de otimização da rede: ADAM
+adam = Adam(lr=0.005, beta_1=0.9, beta_2=0.999)
+# Função de custo baseada em dados originalmente categóricos
+model.compile(loss='categorical_crossentropy', optimizer=adam,
+              metrics=['accuracy'])
+
+
+model.summary()
+
+
+# 
+# 
+# PAREI AQUI!!!!
+# 
+# 
+
+
+# labels = ch_names*570
+# # Convert labels to categorical one-hot encoding
+# one_hot_labels = to_categorical(labels)
+
+# diferenciar controle de não controle 4 caractere pirmeira linha
+# utilizar stardtscaler para normalizar
+
+
+history = model.fit(data, ALCOOL OU NOT []  , epochs=30)
+plot_history(history)
+
+# score = model.evaluate(x_test, y_test, batch_size=128)
+# score = model.predict_classes(X_test)
+# y_true = [np.where(x == 1)[0][0] for x in y_test]
+# print('Acurácia: %0.2f%%' % (accuracy_score(y_true, score) * 100))
+# print('Matriz de confusão:')
+# print(confusion_matrix(y_true, score))
+# print()
+# print(classification_report(y_true, score, digits=5))
+
+
+
+
+
+# tentei assim
+# 
+# 
 # for k in range(0,len(data)):
 #     for i in range(0,len(data[k])):
 #         for j in range(0,len(data[k][i])):
@@ -90,8 +218,7 @@ newData = None
 #     break
 
 
-
-# pessoa1 = data[0][0]
+# pessoa1Trial1 = data[0][0]
 
 # ch_names = ch_names
 # ch_types = ['eeg'] * 64
@@ -101,7 +228,7 @@ newData = None
 #     sfreq=256, 
 #     ch_types=ch_types)
 
-# raw = mne.io.RawArray(pessoa1, info)
+# raw = mne.io.RawArray(pessoa1Trial1, info)
 
 # raw.drop_channels(['x','nd','y'])
 # montage = mne.channels.read_montage('standard_1020')
@@ -121,4 +248,33 @@ newData = None
 # print()
 
 # print("Finalizei com Sucesso!!!")
+
+# Função auxilar que achou a pasta e arquivo com erro na base large (remoção de arquivo necessária)
+# for k in range(0,len(data)):
+#     for i in range(0,len(data[k])):
+#         if(len(data[k][i]) < 64):
+#             print("pasta: ",k)
+#             print("arquivo: ",i)
+#             print("len arquivo: ",len(data[k][i]))
+#             print("data", data[k][i])
+
+
+# Prepara info para os eletrodos selecionados
+# primeira sessão, primeiro trial
+# Média de cada eletrodo (256 -> 1)
+# newData = list()
+# for person in data:
+#     for eletrodos in person:
+#         newEletrodos = list()
+#         for eletrodo in eletrodos:
+#             newEletrodos.append(np.mean(eletrodo))
+#         newData.append(newEletrodos)
+
+# print(np.array(newData).shape)
+
+# TESTE
+# norm1 = normalize(data, axis=1)
+# print(norm1.shape)
+# print(norm1)
+
 
